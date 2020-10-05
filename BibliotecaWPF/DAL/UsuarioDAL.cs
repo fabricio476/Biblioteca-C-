93d3b60c9f61498f1c1f0825c4f964c462676cf4
Fabricio França <40752151+fabricio476@users.noreply.github.com>
2020-10-05 00:51:07 -0300

    adicionado as classes principais do projeto

--- BibliotecaWPF/DAL/UsuarioDAL.cs
+++ BibliotecaWPF/DAL/UsuarioDAL.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace BibliotecaWPF.DAL
 {
-    class Class1
+    class UsuarioDAL
     {
     }
 }
--- BibliotecaWPF/Model/Usuario.cs
+++ BibliotecaWPF/Model/Usuario.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace BibliotecaWPF.Model
 {
-    class Class1
+    class Usuario
     {
     }
 }
